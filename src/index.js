e40f98d81fd86ef75db651122067759289f25590
Philip Basnight <philipbasnight@gmail.com>
2023-06-07 14:14:21 -0400

    index.js reactDOM update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 /**
- * Entry point of application, where App is rendered within the div with the id of "app" 
+ * Entry point of application, where App is rendered within the div with the id of "app"
  */
 
-import React from "react";
-import { render } from "react-dom";
+// import React from "react";
+// import { render } from "react-dom";
 
+// import App from "./App";
+
+// render(<App></App>, document.getElementById("app"));
+
+//reactdom update
+
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-render(<App></App>, document.getElementById("app"));
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
